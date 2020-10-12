5914439a73cb007034b2d4cdc460db577c1c354c
Keshav Mishra <keshav.sm@somaiya.edu>
2020-10-12 22:49:54 +0530

    update Graph algo's building teams solutions

--- Graph Algorithms/building_teams.py
+++ Graph Algorithms/building_teams.py
@@ -5,10 +5,7 @@ class Vertex:
         self.group = 1 
         self.connections = []
 
-
-
-# first we have to see if there is a cycle in the undirected graph (todo) 
-# then we can assign groups to each vertex (done)
+# fails 4 testcases
 def solve():
     n, m = map(int,input().split())
     graph = dict()
@@ -18,22 +15,22 @@ def solve():
         a,b = map(int,input().split())
         graph[a].connections.append(b)
         graph[b].connections.append(a)
-    s = set()
-    flag = False
+    flag = False 
     for i in range(1,n+1):
-        s.add(i)
         for j in graph[i].connections:
-            if j in s and graph[i].group == graph[j].group:
-                flag = True 
-                break
+            if j <= i and graph[i].group == graph[j].group:
+                flag = True
+                break 
             if graph[i].group == graph[j].group:
-                graph[j].group = 0 if graph[i].group else 1 
+                graph[j].group = 0 if graph[i].group else 1
+
     if flag:
         print("IMPOSSIBLE")
-    else:
-        for i in range(1,n+1):
-            print(graph[i].group + 1, end = " ")
-        print() 
-    
+        return 
+
+    for i in range(1,n+1):
+        print(graph[i].group+1,end = " ")
+    print()
+
 if __name__ == '__main__':
     solve() 
