c86d61888bed7c10e68ac5127569d8d75eaedfc2
Keshav Mishra <keshav.sm@somaiya.edu>
2020-10-24 09:18:35 +0530

    update road trip solution

--- Graph Algorithms/round_trip.py
+++ Graph Algorithms/round_trip.py
@@ -2,65 +2,63 @@ from collections import deque
 class Vertex:
     def __init__(self,data):
         self.data = data 
-        self.visited = False 
+        self.color  = 0  
         self.parent = -1 
         self.connections = []
-def printPath(graph,vertex):
-    q = deque()
-    q.append(vertex.data)
-    while(vertex.parent != -1):
-        vertex = graph[vertex.parent]
-        q.appendleft(vertex.data)
-    q.append(q[0])
-    print(len(q))
-    print(*q)
 
-# todo (fails few test cases)
-def bfs(graph,i):
-    q = deque()
-    q.append(i)
-    count = 0
-    while q:
-        count += 1
-        new = deque()
-        for j in q:
-            vertex = graph[j]
-            if vertex.visited and count > 2: return vertex
-            if vertex.visited: continue
-            vertex.visited = True 
-            for k in vertex.connections:
-                if graph[k].visited: continue
-                graph[k].parent = j
-                new.append(k) 
-        q = new
 
-# fails 
+def dfs(graph, v):
+    stack = []
+    stack.append(v)
+    while len(stack):
+        
+        v = stack.pop()
+        graph[v].color = 1 
+        for u in graph[v].connections:
+            if u == graph[v].parent: continue
+            if graph[u].color == 0:
+                graph[u].parent = v 
+                stack.append(u)
+            elif graph[u].color == 1:
+                return [u,v]
+    graph[v].color = 2 
+    return False 
+
+
+def detect_cyle(graph,n):
+    start , end = -1, -1
+    for i in range(1,n+1):
+        if graph[i].color == 0 :
+            k = dfs(graph,i)
+            if k:
+                start,end = k 
+                break
+    if start == -1:
+        print("IMPOSSIBLE")
+    else:
+        path = []
+        path.append(start)
+        while end!=start:
+            path.append(end)
+            end = graph[end].parent
+        path.append(start)
+        print(len(path))
+        print(*path)
+
+
 def solve():
-    n,m = map(int,input().split())
-    graph = dict()
+
+    n, m = map(int, input().split())
+    graph = {}
     for i in range(1,n+1):
         graph[i] = Vertex(i)
-
+    
     for _ in range(m):
         a,b = map(int,input().split())
         graph[a].connections.append(b)
         graph[b].connections.append(a)
     
-    flag = None 
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            graph[j].visited = False
-            graph[j].parent = -1 
-        flag = bfs(graph,i)
-        if flag:
-            break
-    if not flag:
-        print("IMPOSSIBLE")
-        return 
-    print(flag.data)
-    printPath(graph,flag)
-    for i in range(1,n+1):
-        print(graph[i].data,graph[i].parent)
+    detect_cyle(graph,n)
 
 if __name__ == '__main__':
     solve()
